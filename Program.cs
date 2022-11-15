699e898b731701d6306055a7a01955ab945ed257
Galijotas <lgalijotas@gmail.com>
2022-11-15 19:29:00 +0200

    Task Lukas.Galijotas

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace Lukas.Galijotas 
+{
+    internal class Program
+    {
+
+        static void printText(string text)
+        {
+            for (int i = 0; i < text.Length - 1; i++)
+            {
+                int count = 1;
+                while (text[i] == text[i + 1])
+                {
+                    i++;
+                    count++;
+                    if ((i + 1) == text.Length)
+                    {
+                        break;
+                    }
+                    
+                }
+                if (count <= 1)
+                {
+                    Console.Write(text[i]);
+                }
+                else
+                {
+                    Console.Write($"{"{" + count + "" + text[i] + "}"}" + "");
+                }
+                
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"data.txt");
+            printText(text);
+        }
+
+    }
+}
